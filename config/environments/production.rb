08451534320453937363d75c16738b1626de42ed
Thadeu Esteves Jr <tadeuu@gmail.com>
2017-11-14 14:48:09 -0300

    💎

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,8 +53,7 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 20 * 1024)
-  
+  config.logger = ActiveSupport::Logger.new(config.paths['log'].first, 1, 20 * 1024)  
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
